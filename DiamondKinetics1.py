c9b5dfdf91b00dc06c4122cec9a80b83889e0b63
Ary Pathania <arypathania@Arys-MBP.wv.cc.cmu.edu>
2019-11-03 23:18:31 -0500

    finished project

---
+++ DiamondKinetics1.py
@@ -0,0 +1,129 @@
+import csv
+import unittest
+
+
+class SwingData(object):
+    def __init__(self):
+        self.time = []
+        self.axs = []
+        self.ays = []
+        self.azs = []
+        self.wxs = []
+        self.wys = []
+        self.wzs = []
+
+
+
+
+#extrapolate data from csv file, returns allData, a SwingData object comprised
+#of seven data arrays corresponding to time, ax, ay, az, wx, wy, wz.
+def createDataArrayAll(file):
+    with open(file) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        allData = SwingData()
+        for row in csv_reader:
+            allData.time += [row[0]]
+            allData.axs += [row[1]]
+            allData.ays += [row[2]]
+            allData.azs += [row[3]]
+            allData.wxs += [row[4]]
+            allData.wys += [row[5]]
+            allData.wzs += [row[6]]
+        return allData
+
+
+#data is an array created with createDataArrayAll
+
+#start and end are integer values corresponding to the beginning and ending indexes
+
+#thresh is an integer corresponding to the threshold specified
+
+#winL is an integer value corresponding to the number of samples in a row we check against the thresh predicate
+
+def searchContinuityAboveValue(data, start, end, thresh, winL):
+    for i in range(start,end):
+        if data[i] > thresh:
+            satisfies = True
+            for j in  range(winL):
+                if data[i+j] <= thresh or (i+j>end):
+                    satisfies = False
+                    break
+            if satisfies:
+                return i
+    return None
+
+
+#data is one of the sesven arrays created in the createDataArrayAll function
+
+#start and end are integer values corresponding to the beginning and ending indexes
+
+#threshLo is an integer value corresponding to the low end of the threshold
+
+#threshHi is an integer value corresponding to the high end of the threshold
+
+#winL is an integer value corresponding to the number of samples in a row we check against the thresh predicate
+
+def backSearchContinuityWithinRange(data, start, end, threshLo, threshHi, winL):
+    for i in range(start,end):
+        if data[i] > threshLo and data[i] < threshHi:
+            satisfies = True
+            for j in  range(winL):
+                if data[i+j] <= threshLo or data[i+j] >= threshHi or (i+j>end):
+                    satisfies = False
+                    break
+            if satisfies:
+                return i
+    return None
+
+
+#data is an array created with createDataArrayAll
+
+#start and end are integer values corresponding to the beginning and ending indexes
+
+#thresh is a reading object created with int.min for all values except the component being checked
+
+#winL is an integer value corresponding to the number of samples in a row we check against the thresh predicate
+
+def searchContinuityAboveValueTwoSignals(data1, data2, start, end, thresh1, thresh2, winL):
+    for i in range(start,end):
+        if data1[i] > thresh1 and data2[i] > thresh2:
+            satisfies = True
+            for j in  range(winL):
+                if data1[i+j] <= thresh1 or data2[i+j] <= thresh2 or (i+j>end):
+                    satisfies = False
+                    break
+            if satisfies:
+                return i
+    return None
+
+
+#data is an array created with createDataArrayAll
+
+#start and end are integer values corresponding to the beginning and ending indexes
+
+#threshLo is an integer value corresponding to the low end of the threshold
+
+#threshHi is an integer value corresponding to the high end of the threshold
+
+#winL is an integer value corresponding to the number of samples in a row we check against the thresh predicate
+
+def searchMultiContinuityWithinRange(data, start, end, threshLo, threshHi, winLength):
+    currLength = 0
+    inSeq = False
+    startIndex = None
+    ans = []
+    for i in range(start,end):
+        inSeq = False
+        if data[i]>threshLo and data[i]<threshHi:
+           if startIndex == None:
+               startIndex = i
+           currLength+=1
+           inSeq = True
+        if inSeq == False and currLength !=0:
+            if currLength>=winLength:
+                ans.append((startIndex,i-1))
+            currLength = 0
+            startIndex = None
+    return ans
+
+
---
+++ DiamondKineticsTest.py
@@ -0,0 +1,53 @@
+import csv
+import unittest
+import DiamondKinetics1
+
+
+
+
+class SimpleTestCase(unittest.TestCase):
+    def testA(self):
+        data1 = [0,324,4235,6,6,6,100,7,7,8,10,3,7,7,324,1234,23,4,234,24332,412]
+        start = 2
+        end = 12
+        threshLo = 5
+        threshHi = 10
+        winLength = 3
+        print(DiamondKinetics1.searchMultiContinuityWithinRange(data1, start, end, threshLo, threshHi, winLength))
+        assert DiamondKinetics1.searchMultiContinuityWithinRange(data1, start, end, threshLo, threshHi, winLength) == [(3,5),(7,9)]
+
+    def testB(self):
+        data1 = [0,324,4235,6,6,6,100,7,7,8,10,3,7,7,324,1234,23,4,234,24332,412]
+        data2 = [0,324,1,1,1,1,100,7,7,8,10,3,7,7,324,1234,23,4,234,24332,412]
+        start = 2
+        end = 12
+        thresh1 = 5
+        thresh2 = 6
+        winLength = 3
+        print(DiamondKinetics1.searchContinuityAboveValueTwoSignals(data1, data2, start, end, thresh1, thresh2, winLength))
+        assert DiamondKinetics1.searchContinuityAboveValueTwoSignals(data1, data2, start, end, thresh1, thresh2, winLength) == 6
+
+    def testC(self):
+        data = [0,324,4235,6,6,101,100,7,7,8,10,3,7,7,324,1234,23,4,234,24332,412]
+        start = 2
+        end = 12
+        threshLo = 5
+        threshHi = 100
+        winLength = 3
+        print(DiamondKinetics1.backSearchContinuityWithinRange(data, start, end, threshLo, threshHi, winLength))
+        assert DiamondKinetics1.backSearchContinuityWithinRange(data, start, end, threshLo, threshHi, winLength) == 7
+
+    def testD(self):
+        data = [0,324,4235,6,6,101,100,7,7,8,10,3,7,7,324,1234,23,4,234,24332,412]
+        start = 2
+        end = 12
+        thresh= 5
+        winLength = 3
+        print(DiamondKinetics1.searchContinuityAboveValue(data, start, end, thresh, winLength))
+        assert DiamondKinetics1.searchContinuityAboveValue(data, start, end, thresh, winLength) == 2
+
+
+if __name__ == "__main__":
+    data = DiamondKinetics1.createDataArrayAll("latestSwing.csv")
+    print(data.axs)
+    unittest.main()
